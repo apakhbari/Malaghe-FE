73325b2034eee3329288ea4ea4705ceefc4e4298
Rhodium.5135 <apakhbari@gmail.com>
2023-01-08 21:03:03 +0330

    little fix

--- pages/sefaresh/list.js
+++ pages/sefaresh/list.js
@@ -118,40 +118,13 @@ function List({ data }) {
                   </tr>
                 </Fragment>
               ))}
-
-              <tr className="group hover:scale-105 hover:shadow-xl hover:drop-shadow-xl cursor-pointer">
-                <td className=" group-hover:text-primary-focus">تعمیر</td>
-                <td className=" group-hover:text-primary-focus">۴۵۰,۰۰۰</td>
-                <td dir="rtl" className=" group-hover:text-primary-focus">
-                  ۱۰۰۲
-                </td>
-                <th className=" group-hover:text-primary-focus">۱۴۰۱/۰۶/۱۲</th>
-                <th className=" group-hover:text-primary-focus">۲</th>
-              </tr>
-
-              <tr className="group hover:scale-105 hover:shadow-xl hover:drop-shadow-xl cursor-pointer">
-                <td className=" group-hover:text-primary-focus">تعمیر</td>
-                <td className=" group-hover:text-primary-focus">۲۵۰,۰۰۰</td>
-                <td dir="rtl" className=" group-hover:text-primary-focus">
-                  ۱۰۰۳
-                </td>
-                <th className=" group-hover:text-primary-focus">۱۴۰۱/۰۶/۱۴</th>
-                <th className=" group-hover:text-primary-focus">۳</th>
-              </tr>
-
-              <tr className="group hover:scale-105 hover:shadow-xl hover:drop-shadow-xl cursor-pointer">
-                <td className=" group-hover:text-primary-focus">خرید</td>
-                <td className=" group-hover:text-primary-focus">۵۰۰,۰۰۰</td>
-                <td dir="rtl" className=" group-hover:text-primary-focus">
-                  ۱۰۰۴
-                </td>
-                <th className=" group-hover:text-primary-focus">۱۴۰۱/۰۶/۱۶</th>
-                <th className=" group-hover:text-primary-focus">۴</th>
-              </tr>
             </tbody>
           </table>
         ) : (
-          <p3 className="text-neutral-content text-2xl" dir="rtl">
+          <p3
+            className="text-neutral-content text-2xl text-center mt-24"
+            dir="rtl"
+          >
             رکوردی یافت نشد
           </p3>
         )}
