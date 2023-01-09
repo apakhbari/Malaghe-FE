c8b79e8942a1df0a247e780d49e00cf47120081b
Rhodium.5135 <apakhbari@gmail.com>
2023-01-09 12:29:54 +0330

    added icon to workflow

--- pages/sefaresh/[code].js
+++ pages/sefaresh/[code].js
@@ -16,6 +16,18 @@ import RemoveUndefinedsToPleaseNext from '../../hooks/removeUndefineds'
 const OrderCode = ({ data }) => {
   console.log(data)
 
+  const onWorkFlowClick = (e) => {
+    e.preventDefault()
+
+    router.replace(
+      {
+        pathname: `/sefaresh/gardeshkar/${data.code}`,
+        query: { id: data.id },
+      },
+      `/sefaresh/gardeshkar/${data.code}`
+    )
+  }
+
   const orderStatus = {
     1: 'ایجاد شده',
     2: 'در انتظار پرداخت',
@@ -31,7 +43,7 @@ const OrderCode = ({ data }) => {
     <Fragment>
       <Head>
         <title>
-          {CLIENT_NAME_FA} - {data.code}
+          {CLIENT_NAME_FA} - {data.code} سفارش
         </title>
       </Head>
 
@@ -39,8 +51,24 @@ const OrderCode = ({ data }) => {
         <Navbar />
         <CardDescriptionComponent>
           <div className="flex flex-col">
-            <button type="submit" className="btn btn-secondary  mt-2">
+            <button
+              className="flex btn btn-secondary  mt-2 mx-2 text-secondary-content"
+              onClick={onWorkFlowClick}
+            >
               مشاهده گردش کار
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6 fill-secondary-content"
+              >
+                <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                <path
+                  fillRule="evenodd"
+                  d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </button>
 
             <div className="divider">اطلاعات اولیه</div>
